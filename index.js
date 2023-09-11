539f5003e340a622390c28bc6c1416ea78799efe
nurealammiaji <nurealam.miaji@gmail.com>
2023-09-11 22:26:39 +0600

    json data added for recipes and distributed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const recipes = require("./data/recipes.json");
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
     res.send("Recipe Haven Server");
+})
+
+app.get("/recipes", (req, res) => {
+    res.send(recipes);
 })
 
 app.listen(port, () => {
